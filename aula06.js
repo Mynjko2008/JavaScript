09bca7316b9760703d96437cf08c9ee09dcfb50c
Mynjko2008 <amynjko@gmail.com>
2025-05-13 18:39:39 -0300

    javascript

---
+++ aula06.js
@@ -0,0 +1,13 @@
+let cor = "vermelho";
+console.log(cor);
+
+cor = "cinza"; // troquei o valor da variavel para "cinza"
+console.log(cor);
+
+//variáveis do tipo let são total alteraveis, diferentes das variaveis const que não podem ser alteradas pois são constantes
+ 
+//Não podes iniciar uma variavel com NUMEROS
+//Ex: atividade1, exemplo01
+
+//não podemos iniciar uma variavel com espaço ou caracteres especiais
+//não podemos iniciar com nomes reservados ex: let, const, function, if, else etc...
---
+++ aula07.js
@@ -0,0 +1,30 @@
+//Operadores JS
+
+// + = Adição
+// - = Subtração
+// / = Divição
+// * = Multiplicação
+
+const resultadoAdicao = 7 + 45;
+console.log(resultadoAdicao);
+
+const resultadoSubtracao = 50 - 80;
+console.log(resultadoSubtracao);
+
+const resultadoDvisao  = 40/60;
+console.log(resultadoDvisao);
+
+const resultadoMultiplicacao = 20*79;
+console.log(resultadoMultiplicacao);
+// podemos utiliza-los em outras situações, como incrementar uma frase por exemplo
+
+const texto1 = "Olá, Meu Nome é Jonas"
+const texto2 = "Eu tenho 23 anos"
+
+const textoFinal = texto1 + " " + texto2;
+console.log(textoFinal);
+
+let x = 5;
+let y = 2;
+let z = x % y;
+console.log(z)
