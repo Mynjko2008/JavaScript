65ba0c8eaf6f4f1f6782c74da38ec178e18a817c
Mynjko2008 <amynjko@gmail.com>
2025-05-13 21:46:19 -0300

    javascript

---
+++ aula08.js
@@ -0,0 +1,18 @@
+let x = 10;
+let y = 10;
+
+console.log(x === y);
+
+//estou testando se o valor de "X" é igual ao valor de "Y"
+//as respostas vão ser true(verdaderio) e false ( falso)
+
+let frutas = "Banana";
+let frutas2 = "Maça";
+//aqui estou perguntando se o valor da varial " frutas " é diferente da variavel " frutas2 "
+console.log(frutas !==frutas2)
+
+let numero = 20;
+let segundo = 258;
+//estou usando o > ( maior que ) e < (Menor que) para comparar determinas informações
+
+console.log(numero >= 20);
