a88dd94ff39f79c7a16e24227d9d76efa23d68d7
Mynjko2008 <amynjko@gmail.com>
2025-05-16 20:10:26 -0300

    javascript

--- aula09.js
+++ aula09.js
@@ -12,11 +12,11 @@ const carta = {
     ataque: 89,      // <- Número de ATAQUE
     slogan: "Pokémon Amerelo"  // <- Frase especial da carta
 };
-console.log(` meu personagem se chama: ${carta.nome} `);
+//console.log(`Meu personagem se chama: ${carta.nome} `);
 
 const pokemon = {
-    nome: " Charizard ",
-    habilidade: " Blaze ",
+    nome: "Charizard",
+    habilidade: "Blaze",
     hp: 78,
     ataque: 84,
     defesa: 78,
@@ -25,9 +25,25 @@ const pokemon = {
     velocidade: 100,
     slogan: "Lagartão de fogo"
 };
-console.log();
+console.log(`Meu pokémon se chama: ${pokemon.nome}`);
+console.log(`Sua habilidade é: ${pokemon.habilidade}`);
+console.log(`Seu HP é: ${pokemon.hp}`);
+console.log(`Seu ataque é: ${pokemon.ataque}`);
+console.log(`Sua defesa é: ${pokemon.defesa}`);
+console.log(`Seu ataque especial é: ${pokemon.sp_atack}`);
+console.log(`Sua defesa especial é: ${pokemon.sp_def}`);
+console.log(`Sua velocidade é: ${pokemon.velocidade}`);
+console.log(`Seu slogan é: ${pokemon.slogan}`);
 
-const exemplo2 = {
-
+const linguagensProgramacao = {
+    linguagen1:'Java',
+    linguagen2:'PHP',
+    linguagen3:'Python',
+    linguagen4:'C#',
+    linguagen5:'C++'
 };
-console.log();
+console.log(`A linguagem ${linguagensProgramacao.linguagen1} é o inferno na Terra`);
+console.log(`A linguagem ${linguagensProgramacao.linguagen2} é voltada para formularios Back-End`);
+console.log(`A linguagem ${linguagensProgramacao.linguagen3} é perfeita para iniciantes`);
+console.log(`A linguagem ${linguagensProgramacao.linguagen4} é dedicada para aplicativos Windows`);
+console.log(`A linguagem ${linguagensProgramacao.linguagen5} é voltada para programação em jogos`);
