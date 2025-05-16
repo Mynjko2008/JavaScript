38e1f13ffcfa9a825b913dc8beb7da7763b06c36
Mynjko2008 <amynjko@gmail.com>
2025-05-15 21:43:12 -0300

    javascript

--- aula08.js
+++ aula08.js
@@ -4,15 +4,25 @@ let y = 10;
 console.log(x === y);
 
 //estou testando se o valor de "X" é igual ao valor de "Y"
-//as respostas vão ser true(verdaderio) e false ( falso)
+//as respostas vão ser true( verdaderio ) e false ( falso )
 
 let frutas = "Banana";
 let frutas2 = "Maça";
 //aqui estou perguntando se o valor da varial " frutas " é diferente da variavel " frutas2 "
 console.log(frutas !==frutas2)
 
-let numero = 20;
+let numero = 19;
 let segundo = 258;
 //estou usando o > ( maior que ) e < (Menor que) para comparar determinas informações
 
-console.log(numero >= 20);
+console.log(numero <= 19);
+// = ( significa "recebe" - operador de atribuição )
+// Exemplo: let x = 5; ( a variável x RECEBE o valor 5 )
+
+// == ( significa "igual a" - comparação com conversão de tipo )
+// Verifica se os valores são iguais, convertendo os tipos se necessário
+// Exemplo: 5 == "5" retorna true ( número 5 é considerado igual a string "5" )
+
+// === ( significa "idêntico a" - comparação estrita )
+// Verifica se os valores E os tipos são exatamente iguais
+// Exemplo: 5 === "5" retorna false ( número 5 NÃO é idêntico a string "5" )
---
+++ aula09.js
@@ -0,0 +1,33 @@
+//Tipo objeto em javascript
+
+// Isso cria um OBJETO chamado "carta" (como uma ficha de Pokémon)
+const carta = { 
+    // nome: guarda o TIPO da carta (texto)
+    nome: "Pokémon",  // <- Isso é uma PROPRIEDADE
+
+    // habilidade: guarda um NÚMERO (quantos % de habilidade tem)
+    habilidade: 95,   // <- Outra propriedade
+    
+    defesa: 40,      // <- Número de DEFESA
+    ataque: 89,      // <- Número de ATAQUE
+    slogan: "Pokémon Amerelo"  // <- Frase especial da carta
+};
+console.log(` meu personagem se chama: ${carta.nome} `);
+
+const pokemon = {
+    nome: " Charizard ",
+    habilidade: " Blaze ",
+    hp: 78,
+    ataque: 84,
+    defesa: 78,
+    sp_atack: 109,
+    sp_def:100,
+    velocidade: 100,
+    slogan: "Lagartão de fogo"
+};
+console.log();
+
+const exemplo2 = {
+
+};
+console.log();
