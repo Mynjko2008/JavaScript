78f0ad15187624dc501d73b1997b812407b3656c
Mynjko2008 <amynjko@gmail.com>
2025-05-22 21:21:35 -0300

    Javascript

--- atividade pratica/script.js
+++ atividade pratica/script.js
@@ -1,7 +1,3 @@
-//getAttribute - obter o valor de um atributo(valor da linha)
-
-//setAttribute - definir ou modificar o valor de um atributo(valor da linha)
-
 function changeImage() {
     const img = document.getElementById('myImage'); //aqui conseguimos armazenar o conteúdo da imagem
     const currentSrc = img.getAttribute('src'); //aqui pegamos o atributo SRC da imagem, responsável pela alteração da img
@@ -10,11 +6,22 @@ function changeImage() {
         img.setAttribute('src', 'image/jonh.png'); // usamando o setAttribute, alteramos o valor do atributo src para 'image/jonh.png'
         img.setAttribute('alt', 'imagem do Jonh'); // aqui alteramos o valor do atributo alt para 'Jonh'
     } else { //se não, faz o processo inverso 
-        img.setAttribute('src', 'image/arthur.jpg');
-        img.setAttribute('alt', 'imagem do Arthur');
+        img.setAttribute('src', 'image/arthur.jpg'); // aqui usamos o setAttribute para alterar o valor do atributo src para 'image/arthur.jpg'
+        img.setAttribute('alt', 'imagem do Arthur');// aqui alteramos o valor do atributo alt para 'Arthur'
     }
 
 }
 // if = se a condição for valida execute algo
 
 // else = caso contrário / se não for valida execute algo
+
+// else if = se a condição for valida execute algo, se não for valida execute outra coisa
+
+// getAttribute - obter o valor de um atributo(valor da linha)
+
+// setAttribute - definir ou modificar o valor de um atributo(valor da linha)
+
+// src = source (origem) - é o caminho da imagem, onde ela está armazenada
+
+// alt = alternativa - é o texto alternativo que aparece quando a imagem não carrega 
+// ou quando o mouse passa por cima da imagem
